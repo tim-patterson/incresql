6715be0e272704138e6ba9962f00eb7241914fd4
Tim Patterson <tim.wanaka@gmail.com>
2020-08-07 10:30:58 +1200

    Fill in missing aliases in planner

---
+++ src/planner/src/normalize.rs
@@ -0,0 +1,79 @@
+use crate::PlannerError;
+use ast::rel::logical::LogicalOperator;
+
+/// Normalize the query, adding in missing aliases etc so the rest of the planning doesn't need
+/// to work around all of that
+pub fn normalize(mut query: LogicalOperator) -> Result<LogicalOperator, PlannerError> {
+    normalize_impl(&mut query);
+    Ok(query)
+}
+
+fn normalize_impl(query: &mut LogicalOperator) {
+    for child in query.children_mut() {
+        normalize_impl(child);
+    }
+
+    // Column Aliases
+    for (idx, ne) in query.named_expressions_mut().enumerate() {
+        if ne.alias.is_none() {
+            ne.alias = Some(format!("_col{}", idx + 1));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ast::expr::{Expression, NamedExpression};
+    use ast::rel::logical::Project;
+    use data::Datum;
+
+    #[test]
+    fn test_normalize_column_aliases() -> Result<(), PlannerError> {
+        let operator = LogicalOperator::Project(Project {
+            distinct: false,
+            expressions: vec![
+                NamedExpression {
+                    alias: Some(String::from("1")),
+                    expression: Expression::Literal(Datum::Null),
+                },
+                NamedExpression {
+                    alias: None,
+                    expression: Expression::Literal(Datum::Null),
+                },
+            ],
+            source: Box::new(LogicalOperator::Project(Project {
+                distinct: false,
+                expressions: vec![
+                    NamedExpression {
+                        alias: None,
+                        expression: Expression::Literal(Datum::Null),
+                    },
+                    NamedExpression {
+                        alias: None,
+                        expression: Expression::Literal(Datum::Null),
+                    },
+                ],
+                source: Box::new(LogicalOperator::Single),
+            })),
+        });
+
+        let mut normalized = normalize(operator)?;
+        let top_aliases: Vec<_> = normalized
+            .named_expressions_mut()
+            .map(|ne| ne.alias.as_ref().unwrap())
+            .collect();
+
+        assert_eq!(top_aliases, vec!["1", "_col2"]);
+
+        let lower_operator = normalized.children_mut().next().unwrap();
+        let lower_aliases: Vec<_> = lower_operator
+            .named_expressions_mut()
+            .map(|ne| ne.alias.as_ref().unwrap())
+            .collect();
+
+        assert_eq!(lower_aliases, vec!["_col1", "_col2"]);
+
+        Ok(())
+    }
+}
