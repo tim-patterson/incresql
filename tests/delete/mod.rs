59b3be39a46586198cfe90b37bb71abb6fff4c71
Tim Patterson <tim.wanaka@gmail.com>
2020-08-20 14:36:37 +1200

    Add extra test case for delete limit

--- tests/delete/mod.rs
+++ tests/delete/mod.rs
@@ -6,12 +6,14 @@ fn test_delete() {
         connection.query(r#"CREATE TABLE t1 (a INT, b TEXT)"#, "");
 
         connection.query(r#"INSERT INTO t1 SELECT 1, "abc""#, "");
+        connection.query(r#"INSERT INTO t1 SELECT 1, "abc""#, "");
         connection.query(r#"INSERT INTO t1 SELECT 2, "def""#, "");
         connection.query(r#"INSERT INTO t1 SELECT 3, "ghi""#, "");
 
         connection.query(
             r#"SELECT * FROM t1"#,
             "
+            |1|abc|
             |1|abc|
             |2|def|
             |3|ghi|
@@ -24,15 +26,18 @@ fn test_delete() {
             r#"SELECT * FROM t1"#,
             "
             |1|abc|
+            |1|abc|
             |3|ghi|
         ",
         );
 
+        // Check to see that the limit will actually split within a tuple/freq...
         connection.query(r#"DELETE FROM t1 LIMIT 1"#, "");
 
         connection.query(
             r#"SELECT * FROM t1"#,
             "
+            |1|abc|
             |3|ghi|
         ",
         );
